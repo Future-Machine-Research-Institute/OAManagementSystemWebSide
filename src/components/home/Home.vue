ed56242144fc6133b3b7c116d50471903bf25a58
Arrose <arrose.chen@foxmail.com>
2022-11-25 20:19:30 +0800

    init

---
+++ src/components/home/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="content">
+        Hello World!
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+#content {
+    width: 100%;
+    height: 100%;
+    background-color: blueviolet;
+}
+
+</style>
